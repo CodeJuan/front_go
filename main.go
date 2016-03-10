814befe3637f295c894441205574a0c9548513ec
codejuan <xh@decbug.com>
2016-03-10 19:46:03 +0800

    serve filehandle

--- main.go
+++ main.go
@@ -3,14 +3,24 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"log"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
 }
 
-func main(){
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(":80", nil)
+func createHandler() http.Handler {
+	var mux = http.NewServeMux()
+	var fileHandler = http.FileServer(http.Dir("./assets"))
 
+	mux.Handle("/", fileHandler)
+	return mux
+}
+
+func main(){
+	handler := createHandler()
+	if err := http.ListenAndServe("0.0.0.0:80", handler); err != nil {
+		log.Fatal(err)
+	}
 }
