c8da894f6f61cc1bacdbdce063172f88daf1ff0e
codejuan <xh@decbug.com>
2016-03-10 20:14:51 +0800

    ng-view

---
+++ assets/js/app.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Administrator on 2016/3/10 0010.
+ */
+var testApp = angular.module('testApp', ['ngRoute']);
+
+testApp.config(function($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: 'html/test.html',
+            controller: 'testController'
+        });
+});
+
+testApp.controller('testController', function($scope) {
+    $scope.pageClass = 'test';
+});
